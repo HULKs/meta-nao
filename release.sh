29508293fcda598d4ae8ed34c8368813f25b0f9f
Maximilian Schmidt <maximilian@schmidt.so>
2024-03-17 16:41:16 +0000

    Adjust release script to meta-hulks merge

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-TAG=$(grep -P 'DISTRO_VERSION\s*=\s*".+"' conf/distro/HULKs-OS.conf | cut -d\" -f2)
+TAG=$(grep -P 'DISTRO_VERSION\s*=\s*".+"' meta-hulks/conf/distro/HULKs-OS.conf | cut -d\" -f2)
 git tag ${TAG}
 echo "Created tag ${TAG}"
 echo "Please check that the tag is correct and then execute:"
